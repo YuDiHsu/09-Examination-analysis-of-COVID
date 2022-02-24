a97f940ac90413efcbb93d25f1ae3480f07244b7
YuDiHsu <46337820+YuDiHsu@users.noreply.github.com>
2022-02-24 22:22:49 +0800

    Update Optimize_efficiency_of_examination .py

--- Optimize_efficiency_of_examination .py
+++ Optimize_efficiency_of_examination .py
@@ -17,11 +17,11 @@ import re
 
 
 def download_raw_data(file_name, code):
-    dsn = cx_Oracle.makedsn('192.168.170.52', '1561', service_name='DW')
+    dsn = cx_Oracle.makedsn('IP', 'port', service_name='DW')
 
     conn = cx_Oracle.connect(
-        user='sas',
-        password='ueCr5brAD6u4rAs62t9a',
+        user='username',
+        password='pwd',
         dsn=dsn,
         encoding='UTF8',
         nencoding='UTF8'
